ef7e30f42a7622fd1b0f2231c7739e6931cc7d4b
Yuki Hayashi <mrchildreny@gmail.com>
2025-05-08 10:17:54 +0900

    ReactQueryProviderコンポーネント内でQueryClientの初期化方法を変更し、useStateフックを使用してクライアントを管理するようにしました。これにより、状態管理が改善され、コードの可読性が向上しました。

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,16 +1,18 @@
 import { useAppState } from '@/hooks/useAppState';
 import { useOnlineManager } from '@/hooks/useOnlineManager';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export const ReactQueryProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   useAppState();
   useOnlineManager();
 
-  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
